c707178d1805114ef5f8f6c3e2f90753dbd70227
Morten Kolstad <morteako@student.matnat.uio.no>
2021-08-21 01:55:58 +0200

    add parse-file

---
+++ src/Parse.hs
@@ -0,0 +1,21 @@
+module Parse where
+
+import Data.List (isPrefixOf)
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
+import Term (Term (..))
+
+parseType :: String -> Maybe Term
+parseType str =
+  case splitOn " ::" str of
+    termName : typeStr : _ -> Just $ Term {_name = termName, _context = "", _termType = typeStr}
+    _ -> Nothing
+
+-- fix fully qualified aka remove
+parseBrowse :: [String] -> [Term]
+parseBrowse = mapMaybe parseType . filter isNormalTerm
+  where
+    isNormalTerm x = all ($ x) [notNewline, notTypeAlias, notTypeClass]
+    notNewline = not . isPrefixOf " "
+    notTypeAlias = not . isPrefixOf "type "
+    notTypeClass = not . isPrefixOf "class "
